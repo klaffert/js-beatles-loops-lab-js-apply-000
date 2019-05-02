fdff80ffa0b9e969a90a85235fd22ba826d1692b
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 01:13:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musician, instrument) {
   var array = []
-  for (var i = 1; i < 4; i++)
+  for (var i = 1; i < 1 i++)
   array.push(`${musician} plays ${instrument[i]}`)
   return `${musician} plays ${instrument} ${array.join(", ")}`
 }
