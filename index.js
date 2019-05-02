48ee83838e8074187abc531750c9def6661add3b
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 01:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musician, instrument) {
   var array = []
-  for (var i = 1; i < 4; i++) 
-  return `${musician} plays`
+  for (var i = 1; i < 4; i++)
+  array.line(`${musicians} plays ${instrument[i]}`)
+  return `${musician} plays ${instrument}`
 }
