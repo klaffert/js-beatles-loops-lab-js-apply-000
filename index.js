b56290e04113b7fba1f6f9c84872f9986d2365c4
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 00:58:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musician, instrument) {
   var array = []
   for (var i = 0; i < 5; i++) 
+  return `${musician} plays ${instrument}`
 }
