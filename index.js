81cc1b776bb3cee716973991bcc5eddd97985925
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 01:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function theBeatlesPlay(musicians,instruments) {
   return array
 }
 
-function johnLennonFacts (factsarray) {
+function johnLennonFacts (factsArray) {
   const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ]
- while ()
+ while (facts
 }
