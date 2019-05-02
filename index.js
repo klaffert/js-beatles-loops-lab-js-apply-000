26d07b1bf12c98f737d34d990a3958b4cc2849af
Kelliann Lafferty <kell10192@gmail.com>
2019-05-02 01:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function johnLennonFacts (jLFacts) {
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ]
-while (facts > 2
+while (facts > 2)
 facts.push("!!!") 
 return facts
 }
